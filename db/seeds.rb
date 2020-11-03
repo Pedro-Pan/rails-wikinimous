4f7579ff348bd72a05bdd5ea145fa8e0b8058ecd
Pedro Vilarinho <Pedro.Vilarinho.a@outlook.com>
2020-11-03 16:57:54 +0100

    Been helping my buddy so no styling for this one either

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "creating articles"
+10.times do
+  Article.create(title: "#{Faker::FunnyName.name} padawan", content: "#{Faker::Quote.yoda}")
+end
